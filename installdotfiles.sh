543d58495d24fdba20e08f81f62e84d1a97694ed
Giulia Vergottini <hello@railslide.io>
2014-07-07 22:37:57 +0200

    add sublime to the install script

--- installdotfiles.sh
+++ installdotfiles.sh
@@ -7,9 +7,8 @@
 
 dir=~/dotfiles
 olddir=~/dotfiles_old
-files="bashrc gitignore_global gitconfig"
-
-####
+files="bashrc gitignore_global gitconfig" 	#files to symlink
+sublimedir=~/.config/sublime-text-3/Packages/User
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
@@ -23,11 +22,23 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    if [ -a  ~/.$file ] # check if a dotfile already exists
-        then
-            echo "Moving any existing dotfiles from ~ to $olddir"
-            mv ~/.$file ~/dotfiles_old/
+    if [ -a  ~/.$file ]; then	 # check if a dotfile already exists
+        echo "Moving any existing dotfiles from ~ to $olddir"
+        mv ~/.$file $olddir
     fi
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+echo "...done"
+
+# move Sublime Text Users directory to dotfiles_old directory, then create symlink
+if [ -d $sublimedir ]; then	# check whether the directory already exists
+    echo "Moving the existing Sublime Text Users directory from $sublimedir to $olddir"
+    mv $sublimedir $olddir
+    echo "...done"
+fi
+echo "Creating symlink to Users in $sublimedir"
+ln -s $dir/sublime/Users $sublimedir
+echo "...done"
+
