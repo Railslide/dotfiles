bbe3d982e9fdc94f6acc388c26a5690bcd0352fb
Giulia Vergottini <hello@railslide.io>
2022-12-29 17:05:12 +0100

    Add packer as plugin manager

---
+++ neovim/.config/nvim/lua/config/plugin-manager.lua
@@ -0,0 +1,35 @@
+-- Auto install packer if not installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+-- Reload Neovim and installs/updates/removes plugins when this file is saved
+vim.cmd([[ 
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost packer.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Installed plugins
+return require('packer').startup(function(use)
+    
+  use 'wbthomason/packer.nvim'
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  -- Automatically set up configurations if packer was newly installed
+  -- This needs to be at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
