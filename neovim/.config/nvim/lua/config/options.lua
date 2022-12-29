9dbda10207a402b1bd9c78126a551fe7fabf486d
Giulia Vergottini <hello@railslide.io>
2022-12-29 12:48:49 +0100

    Move options to lua

---
+++ neovim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Show line numbers
+vim.opt.number = true
+
+-- Search settings
+vim.opt.ignorecase = true -- ignore case when searching
+vim.opt.smartcase = true -- respect case if there are capital letters in the search string
+
+-- Tabs and indentation
+vim.opt.expandtab = true -- convert tab to spaces
+vim.opt.softtabstop = 4 -- 4 spaces for tab
+vim.opt.shiftwidth = 4 -- 4 spaces for autoindent 
