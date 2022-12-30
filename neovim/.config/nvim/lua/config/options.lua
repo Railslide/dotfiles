d98b4f66fc7905939433dbf9306a2a43949be814
Giulia Vergottini <hello@railslide.io>
2022-12-30 17:40:54 +0100

    Add split options + default to 2 spaces

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -7,5 +7,9 @@ vim.opt.smartcase = true -- respect case if there are capital letters in the sea
 
 -- Tabs and indentation
 vim.opt.expandtab = true -- convert tab to spaces
-vim.opt.softtabstop = 4 -- 4 spaces for tab
-vim.opt.shiftwidth = 4 -- 4 spaces for autoindent 
+vim.opt.softtabstop = 2 -- default to 2 spaces for tab
+vim.opt.shiftwidth = 2 -- default to 2 spaces for autoindent
+
+-- Split windows
+vim.opt.splitright = true -- create vertical splits on the right o the current one
+vim.opt.splitbelow = true -- create horizontal splits below the current one
