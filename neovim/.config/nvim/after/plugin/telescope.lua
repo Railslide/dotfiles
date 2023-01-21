bd7a962c982be1068a290fb9637517dd4350b66a
Giulia Vergottini <hello@railslide.io>
2023-01-21 16:18:06 +0100

    Include hidden files and directories in Telescope scope

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,43 @@
-local status_ok, telescope_builtin = pcall(require, 'telescope.builtin')
+local status_ok, telescope = pcall(require, 'telescope')
 if not status_ok then
   print 'Could not find Telescope plugin'
+  return
+end
+
+--[[
+live_grep and grep_string don't have a `find_command` option
+so we need to do this whole dance in order to include hidden
+files and directories.
+]]
+local status_ok, telescope_config = pcall(require, 'telescope.config')
+if not status_ok then
+  print 'Could not find Telescope config'
+  return
+end
+
+local vimgrep_arguments = { unpack(telescope_config.values.vimgrep_arguments) }
+
+-- Include hidden files and directories, but ignore the `.git` directory.
+table.insert(vimgrep_arguments, "--hidden")
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup({
+  defaults = {
+    vimgrep_arguments = vimgrep_arguments,
+  },
+  pickers = {
+    find_files = {
+      -- Include hidden files and directories, but ignore the `.git` directory.
+      find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+    },
+  },
+})
+
+-- Custom key mappings
+local status_ok, telescope_builtin = pcall(require, 'telescope.builtin')
+if not status_ok then
+  print 'Could not find Telescope builtin'
   return
 end
 
