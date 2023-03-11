eadffa5196eac17c9f889ed740783846fdcdcbbf
Giulia Vergottini <hello@railslide.io>
2023-03-11 22:47:16 +0100

    Attach function for lsp

--- neovim/.config/nvim/after/plugin/lsp/lspconfig.lua
+++ neovim/.config/nvim/after/plugin/lsp/lspconfig.lua
@@ -1,7 +1,26 @@
-local status, lspconfig = pcall(require, "lspconfig")
+local status, lspconfig = pcall(require, 'lspconfig')
 if not status then
   return
 end
 
-lspconfig.pyright.setup{}
-lspconfig.sumneko_lua.setup{}
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+end
+
+lspconfig['pyright'].setup{
+  on_attach = on_attach,
+}
+
+lspconfig['lua_ls'].setup{
+  on_attach = on_attach,
+  -- server-specific settings
+  settings = {
+    Lua = {
+      -- make the language server recognize 'vim' global
+      diagnostics = {
+        globals = { 'vim' },
+      },
+    }
+  }
+}
