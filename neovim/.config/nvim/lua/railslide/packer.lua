4b8eb970e150ca6fafeef2d27ed443f7ecf964d0
Giulia Vergottini <hello@railslide.io>
2023-06-26 22:46:57 +0200

    Automatically update Telescope

--- neovim/.config/nvim/lua/railslide/packer.lua
+++ neovim/.config/nvim/lua/railslide/packer.lua
@@ -35,7 +35,7 @@ return require('packer').startup(function(use)
 
   -- Find all the things
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    'nvim-telescope/telescope.nvim', branch = '0.1.x',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
