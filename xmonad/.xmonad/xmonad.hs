83f8e214b9ce7770caaebbfe7a616587bc5620c4
Giulia Vergottini <hello@railslide.io>
2023-08-25 17:32:01 +0200

    xmonad basic config

---
+++ xmonad/.xmonad/xmonad.hs
@@ -0,0 +1,23 @@
+import XMonad
+
+
+myTerminal = "alacritty"
+
+-- Use Super as the mod key
+myModMask  = mod4Mask
+
+-- Actions to perform whenever xmonad starts or is restarted
+myStartupHook = do
+  -- TODO: add a shortcut for switching layouts
+  spawn "setxkbmap en,se,it"
+
+
+-- Run xmonad with the settings specified above
+main :: IO ()
+main = xmonad $ def
+  {
+    terminal    = myTerminal,
+    modMask     = myModMask,
+    startupHook = myStartupHook
+  }
+
