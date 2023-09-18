8513b971f29e188d249d5c8ca26093862515b21e
Giulia Vergottini <hello@railslide.io>
2023-09-18 22:53:52 +0200

    Lazy load + check for updates

--- neovim/.config/nvim/lua/railslide/lazy.lua
+++ neovim/.config/nvim/lua/railslide/lazy.lua
@@ -11,7 +11,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  { import = "railslide.plugins" },
-  { import = "railslide.plugins.lsp" },
-})
+require("lazy").setup(
+  {
+    { import = "railslide.plugins" },
+    { import = "railslide.plugins.lsp" },
+  },
+  -- Lazy config
+  {
+    checker = {
+      enabled = true,
+    }
+  }
+)
