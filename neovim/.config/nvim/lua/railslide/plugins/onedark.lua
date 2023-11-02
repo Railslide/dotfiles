dd6ca0691b8e505e886da9b2f23f881a381c5144
Giulia Vergottini <hello@railslide.io>
2023-11-02 14:34:49 +0100

    Set manually colorscheme instead of loading it

--- neovim/.config/nvim/lua/railslide/plugins/onedark.lua
+++ neovim/.config/nvim/lua/railslide/plugins/onedark.lua
@@ -3,11 +3,10 @@ return {
     'navarasu/onedark.nvim',
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      local onedark = require('onedark')
-      onedark.setup {
+      require('onedark').setup {
         style = 'darker'
       }
-      onedark.load()
+      vim.cmd.colorscheme 'onedark'
     end,
   },
 }
