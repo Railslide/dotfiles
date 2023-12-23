cd96cfea3b45879b31c3e3bd00671ca42022b58b
Giulia Vergottini <hello@railslide.io>
2023-12-23 16:57:35 +0100

    Add bash check

--- check_dependencies.sh
+++ check_dependencies.sh
@@ -1,4 +1,8 @@
 # Helper functions to print consistent messages
+printstep()
+{
+  echo -e "\nChecking $1..."
+}
 printerror()
 {
   echo -e "- \033[31m$1\033[0m"
@@ -10,12 +14,20 @@ printok()
 }
 
 # bash
+printstep "Bash settings"
+if grep .bash_custom ~/.bashrc &> /dev/null; then
+  printok ".bash_custom sourcing"
+else
+  printerror ".bash_custom is not sourced in .bashrc!"
+fi
+
 # neovim
 if command -v nvim &> /dev/null; then
-  echo -e '\nChecking Neovim dependencies...'
+  printstep "Neovim dependencies"
 
   min_nvim_version="v0.8.0"
   current_nvim_version=$(nvim --version | grep -Eo "v[0-9]+\.[0-9]+\.[0-9]+")
+
   if [ $(echo -e "${min_nvim_version}\n${current_nvim_version}" | sort | head -1) != "${min_nvim_version}" ]; then
     printerror "Unsupported version of Neovim found!"
   else
@@ -31,7 +43,7 @@ fi
 
 # alacritty
 if command -v alacritty &> /dev/null; then
-  echo -e "\nChecking Alacritty dependencies..."
+  printstep "Alacritty dependencies"
 
   if fc-list | grep SauceCodePro &> /dev/null; then
     printok "Fonts"
