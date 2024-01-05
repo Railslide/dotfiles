9a43d89ce065ef53d74df59187b452285b3b14d4
Giulia Vergottini <hello@railslide.io>
2024-01-05 16:27:36 +0100

    Lualine tweaks
    - Remove git info
    - Remove separators
    - Move diagnostics to the rights

--- neovim/.config/nvim/lua/railslide/plugins/lualine.lua
+++ neovim/.config/nvim/lua/railslide/plugins/lualine.lua
@@ -11,13 +11,20 @@ return {
     vim.opt.showmode = false
 
     lualine.setup({
+      options = {
+        component_separators = '',
+        section_separators = '',
+      },
       sections = {
+        lualine_b = {'filename'},
+        lualine_c = {},
         lualine_x = {
           {
             lazy_status.updates, -- Show if there are available updates for plugins
             cond = lazy_status.has_updates,
             color = { fg = "#ff9e64" },
           },
+          {'diagnostics'},
           {'encoding'},
           {'fileformat'},
           {'filetype'},
