8ddd0ec4d45a77171a30bfd405e1a6d421db90c5
Giulia Vergottini <hello@railslide.io>
2024-02-04 22:48:30 +0100

    Make media keys work + add some utils

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,23 +1,50 @@
 import XMonad
 
+-- Fullscreen and EWMH compatibility
+import XMonad.Hooks.EwmhDesktops
 
+-- Keybindings
+import XMonad.Util.EZConfig
+import Graphics.X11.ExtraTypes.XF86
+
+-- Status bar
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
+
+myModMask  = mod4Mask  -- Use Super as mod key
 myTerminal = "alacritty"
-
--- Use Super as the mod key
-myModMask  = mod4Mask
 
 -- Actions to perform whenever xmonad starts or is restarted
 myStartupHook = do
   -- TODO: add a shortcut for switching layouts
   spawn "setxkbmap -layout us,se,it"
 
+myKeyBindings =
+  [ ("M-f", spawn "firefox")
+  -- Media keys
+  , ("<XF86AudioPlay>", spawn "playerctl play-pause")
+  , ("<XF86AudioNext>", spawn "playerctl next")
+  , ("<XF86AudioPrev>", spawn "playerctl previous")
+  ]
+  -- 121 XF86AudioMute
+  -- 122 XF86AudioLowerVolume
+  -- 123 XF86AudioRaiseVolume
+  -- 107 Print
+  -- 119 Delete
+  -- 237 XF86KbdBrightnessDown
+  -- 238 XF86KbdBrightnessUp
+  -- 232 XF86MonBrightnessDown
+  -- 233 XF86MonBrightnessUp
 
--- Run xmonad with the settings specified above
-main :: IO ()
-main = xmonad $ def
+myConfig = def
   {
     terminal    = myTerminal,
     modMask     = myModMask,
     startupHook = myStartupHook
-  }
+  } `additionalKeysP` myKeyBindings
 
+-- Run xmonad with the settings specified above
+main :: IO ()
+main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
