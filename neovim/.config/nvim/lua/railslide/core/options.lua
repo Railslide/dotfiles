f1c499fb49830f04062d8b2b811899bc0d93a5ea
Giulia Vergottini <hello@railslide.io>
2024-05-24 16:01:56 +0200

    Fix python indentation

--- neovim/.config/nvim/lua/railslide/core/options.lua
+++ neovim/.config/nvim/lua/railslide/core/options.lua
@@ -6,10 +6,21 @@ vim.opt.signcolumn = "yes" -- always show column sign to prevent text from jumpi
 vim.opt.ignorecase = true -- ignore case when searching
 vim.opt.smartcase = true  -- respect case if there are capital letters in the search string
 
--- Tabs and indentation
+-- Tabs and general indentation
 vim.opt.expandtab = true -- convert tab to spaces
 vim.opt.softtabstop = 2  -- default to 2 spaces for tab
 vim.opt.shiftwidth = 2   -- default to 2 spaces for autoindent
+
+-- Python indentation
+-- See: https://neovim.io/doc/user/indent.html#ft-python-indent
+vim.g.python_indent = {
+  disable_parentheses_indenting = false,
+  closed_paren_align_last_line = false,
+  continue = "shiftwidth()",
+  searchpair_timeout = 150,
+  open_paren = "shiftwidth()",
+  nested_paren = "shiftwidth()",
+}
 
 -- Split windows
 vim.opt.splitright = true -- create vertical splits on the right of the current one
