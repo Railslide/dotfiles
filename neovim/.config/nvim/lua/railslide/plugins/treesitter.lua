01487cfbf94ee828062f7727dae63fca824d783e
Giulia Vergottini <hello@railslide.io>
2024-12-04 14:52:12 +0100

    Extend treesitter config

--- neovim/.config/nvim/lua/railslide/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/railslide/plugins/treesitter.lua
@@ -5,11 +5,25 @@ return {
 
   config = function ()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "go", "lua", "python" },
+      ensure_installed = {
+        "go",
+        "html",
+        "lua",
+        "markdown",
+        "python"
+      },
       highlight = {
         enable = true,
       },
-
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-CR>",
+          node_incremental = "<C-CR>",
+          scope_incremental = false,
+          node_decremental = "<BS>",
+        },
+      },
     })
   end
 }
