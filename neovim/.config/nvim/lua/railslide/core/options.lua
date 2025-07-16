29bcd59530652cba7bf7d359e1fcc6a4e5448f64
Giulia Vergottini <hello@railslide.io>
2025-07-16 14:46:50 +0200

    Consistent capitalization

--- neovim/.config/nvim/lua/railslide/core/options.lua
+++ neovim/.config/nvim/lua/railslide/core/options.lua
@@ -1,15 +1,15 @@
 -- Appearance
 vim.opt.number = true      -- Show line numbers
-vim.opt.signcolumn = "yes" -- always show column sign to prevent text from jumping left/right
+vim.opt.signcolumn = "yes" -- Always show column sign to prevent text from jumping left/right
 
 -- Search settings
-vim.opt.ignorecase = true -- ignore case when searching
-vim.opt.smartcase = true  -- respect case if there are capital letters in the search string
+vim.opt.ignorecase = true -- Ignore case when searching
+vim.opt.smartcase = true  -- Respect case if there are capital letters in the search string
 
 -- Tabs and general indentation
-vim.opt.expandtab = true -- convert tab to spaces
-vim.opt.softtabstop = 2  -- default to 2 spaces for tab
-vim.opt.shiftwidth = 2   -- default to 2 spaces for autoindent
+vim.opt.expandtab = true -- Convert tab to spaces
+vim.opt.softtabstop = 2  -- Default to 2 spaces for tab
+vim.opt.shiftwidth = 2   -- Default to 2 spaces for autoindent
 
 -- Python indentation
 -- See: https://neovim.io/doc/user/indent.html#ft-python-indent
@@ -22,12 +22,15 @@ vim.g.python_indent = {
   nested_paren = "shiftwidth()",
 }
 
+-- Disable Python provider, so that it doesn't try to search in all
+vim.g.loaded_python3_provider = 0
+
 -- Split windows
-vim.opt.splitright = true -- create vertical splits on the right of the current one
-vim.opt.splitbelow = true -- create horizontal splits below the current one
+vim.opt.splitright = true -- Create vertical splits on the right of the current one
+vim.opt.splitbelow = true -- Create horizontal splits below the current one
 
 -- Clipboard
-vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+vim.opt.clipboard:append("unnamedplus") -- Use system clipboard as default register
 
 -- Motions
 vim.opt.iskeyword:append({ "-" }) -- Threat hyphenated words as a single word
